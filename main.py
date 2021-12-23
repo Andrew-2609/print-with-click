73d3faa2e9b913080fc0f6106d60a583de7e0bd3
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-23 18:21:18 -0300

    Feat: take screenshot at every left mouse button click with Screenshooter

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 from pynput.mouse import Listener, Button
+
+from screenshot import Screenshooter
+
+my_screenshooter = Screenshooter(0)
 
 
 def on_click(x, y, button, pressed):
     if pressed and button == Button.left:
-        print(x, y)
-        print("Mouse clicked.")
+        my_screenshooter.take_screenshot()
+        print(f"Printed at {datetime.now()} when you clicked at ({x}, {y})")
 
 
 with Listener(on_click=on_click) as listener:
