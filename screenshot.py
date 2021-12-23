1ce39413712d2504f41822049397b37aa6584114
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-23 18:31:46 -0300

    Feat: make the screenshots path a parameter of Screenshooter class

--- screenshot.py
+++ screenshot.py
@@ -2,9 +2,10 @@ import pyautogui
 
 
 class Screenshooter:
-    def __init__(self, count):
+    def __init__(self, count, screenshots_path):
         self.count = count
+        self.screenshots_path = screenshots_path
 
     def take_screenshot(self):
-        pyautogui.screenshot().save(f"screenshots/screenshot{self.count}.png")
+        pyautogui.screenshot().save(f"{self.screenshots_path}/screenshot{self.count}.png")
         self.count += 1
